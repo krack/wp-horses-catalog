9cec02cda4ded954e400eea5d7c661ac4908771c
Sylvain gandon <krack_6@hotmail.com>
2020-03-18 14:39:20 +0100

    horse card : fix missing /dib

--- template/sf-judges.php
+++ template/sf-judges.php
@@ -65,76 +65,77 @@
 
         <?php if(isYoungHorse($horse)){ ?>
             <div class="cso list-note">
-            <h3><?php _e("Ability to obstacle", 'horses-catalog') ?></h3>
-            <div>
-                <div class="notation">
-                    <span class="label"><?php _e("Balance on jumps", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleEquilibre; ?></span>
-                </div>
-                <div class="notation">
-                    <span class="label"><?php _e("Means on the obstacle", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleResource; ?></span>
-                </div>
-                <div class="notation">
-                    <span class="label"><?php _e("Style on the obstacle", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleStyle; ?></span>
-                </div>
-                <div class="notation doublelines">
-                    <span class="label"><?php _e("Blood, will, respect & bar intelligence", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleRespect; ?></span>
+                <h3><?php _e("Ability to obstacle", 'horses-catalog') ?></h3>
+                <div>
+                    <div class="notation">
+                        <span class="label"><?php _e("Balance on jumps", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleEquilibre; ?></span>
+                    </div>
+                    <div class="notation">
+                        <span class="label"><?php _e("Means on the obstacle", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleResource; ?></span>
+                    </div>
+                    <div class="notation">
+                        <span class="label"><?php _e("Style on the obstacle", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleStyle; ?></span>
+                    </div>
+                    <div class="notation doublelines">
+                        <span class="label"><?php _e("Blood, will, respect & bar intelligence", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->freeObstacleRespect; ?></span>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php } ?>
 
         <?php if(!isYoungHorse($horse)){ ?>
-        <div class="freejump">
-            <h3><?php _e("Jumping ability", 'horses-catalog') ?></h3>
-            <div>
-                <span class="value"><?php echo $horse->notes->sfExprets->freeObstacle; ?></span>
-                <pre><?php echo $horse->notes->sfExprets->freeObstacleComment; ?></pre>
-            </div>
-        </div>
-        <?php 
-        function isRidingObstacleEmpty(){
-            global $horse;
-            $isEmpty = (($horse->notes->sfExprets->ridingObstacleEquilibre) == null);
-            $isEmpty &= (($horse->notes->sfExprets->ridingObstacleResource) == null);
-            $isEmpty &= (($horse->notes->sfExprets->ridingObstacleStyle) == null);
-            $isEmpty &= (($horse->notes->sfExprets->ridingObstacleRespect) == null);
-            $isEmpty &= (($horse->notes->sfExprets->ridingObstacleComment) == null);
-
-            
-            return $isEmpty;
-        }
-        if(!isRidingObstacleEmpty()){
-        ?>
-        <div class="cso list-note">
-            <h3><?php _e("Ability to climb jump obstacle", 'horses-catalog') ?></h3>
-            <div>
-                <div class="notation">
-                    <span class="label"><?php _e("Balance on jumps", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleEquilibre; ?></span>
+            <div class="freejump">
+                <h3><?php _e("Jumping ability", 'horses-catalog') ?></h3>
+                <div>
+                    <span class="value"><?php echo $horse->notes->sfExprets->freeObstacle; ?></span>
+                    <pre><?php echo $horse->notes->sfExprets->freeObstacleComment; ?></pre>
                 </div>
-                <div class="notation">
-                    <span class="label"><?php _e("Means on the obstacle", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleResource; ?></span>
-                </div> 
-                <div class="notation">
-                    <span class="label"><?php _e("Style on the obstacle", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleStyle; ?></span>
-                </div> 
-                <div class="notation doublelines">
-                    <span class="label"><?php _e("Blood, will, respect & bar intelligence", 'horses-catalog') ?></span>
-                    <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleRespect; ?></span>
-                </div> 
-                <pre><?php echo $horse->notes->sfExprets->ridingObstacleComment; ?></pre>
             </div>
-        </div>
-        <?php } ?>
-    </div>
+            <?php 
+            function isRidingObstacleEmpty(){
+                global $horse;
+                $isEmpty = (($horse->notes->sfExprets->ridingObstacleEquilibre) == null);
+                $isEmpty &= (($horse->notes->sfExprets->ridingObstacleResource) == null);
+                $isEmpty &= (($horse->notes->sfExprets->ridingObstacleStyle) == null);
+                $isEmpty &= (($horse->notes->sfExprets->ridingObstacleRespect) == null);
+                $isEmpty &= (($horse->notes->sfExprets->ridingObstacleComment) == null);
 
-    <?php } ?>
+                
+                return $isEmpty;
+            }
+            if(!isRidingObstacleEmpty()){
+            ?>
+            <div class="cso list-note">
+                <h3><?php _e("Ability to climb jump obstacle", 'horses-catalog') ?></h3>
+                <div>
+                    <div class="notation">
+                        <span class="label"><?php _e("Balance on jumps", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleEquilibre; ?></span>
+                    </div>
+                    <div class="notation">
+                        <span class="label"><?php _e("Means on the obstacle", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleResource; ?></span>
+                    </div> 
+                    <div class="notation">
+                        <span class="label"><?php _e("Style on the obstacle", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleStyle; ?></span>
+                    </div> 
+                    <div class="notation doublelines">
+                        <span class="label"><?php _e("Blood, will, respect & bar intelligence", 'horses-catalog') ?></span>
+                        <span class="value"><?php echo $horse->notes->sfExprets->ridingObstacleRespect; ?></span>
+                    </div> 
+                    <pre><?php echo $horse->notes->sfExprets->ridingObstacleComment; ?></pre>
+                </div>
+            </div>
+            <?php } ?>
+
+        <?php } ?>
+
+    </div>
 
     <?php if(isYoungHorse($horse)){ ?>
     <div class="global-impression">
