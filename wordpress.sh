7284314a3d575057e9bca1499e389e02b2407807
Gandon Sylvain <krack_6@hotmail.com>
2022-12-17 13:24:27 +0000

    use wp cli

--- wordpress.sh
+++ wordpress.sh
@@ -1,1 +1,9 @@
- wp core download --locale=fr_FR --path=/workspace/wordpress/
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+
+wp core download --locale=fr_FR --path=/workspace/wordpress/
+wp db create
+wp core install --url=wpclidemo.dev --title="Etalon SF" --admin_user=admin --admin_password=admin --admin_email=admin@test.com
+wp plugin update --all
